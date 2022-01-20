c5b89994dd133a2545bc7161615f51e00e19d006
amit-tzadok <amit.tzadok@icloud.com>
2022-01-19 20:04:09 -0500

    Added prework files to repository

---
+++ Prework/SettingsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SettingsViewController.swift
+//  Prework
+//
+//  Created by Amit Tzadok on 1/19/22.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Sets the title in the Navigation Bar
+        self.title = "Tip Calculator"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
